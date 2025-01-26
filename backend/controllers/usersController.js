a1ff96362c94ac2df12b6097ff3e5be4087d626d
Kessouri Chaouki <107209548+chaoukiksr@users.noreply.github.com>
2025-01-26 12:37:46 +0100

    refrechToken
    I added the refrech Token to the login endpoint to ensure the security of the login process, and insure generating a new JWT fot each user smoothly without needing to log themout and in

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -4,9 +4,10 @@ import hashData from "../utils/hashData.js"
 import sendMail from "../utils/mailService.js"
 import redisClient from "../server.js"
 import bcrypt from 'bcrypt'
-import {generateJWT,verifyJWT} from '../utils/JWT.js'
+import generateSec from '../utils/jwtSecretGen.js'
+import { generateJWT, verifyJWT } from '../utils/JWT.js'
 export default {
-  
+
   registreUser: async (req, res) => {
 
     const { name, email, password } = req.body
@@ -23,8 +24,8 @@ export default {
       let otpCode = await generateOTP()
       let hashedOtp = await hashData(otpCode.toString())
       let redisKey = `otp:user:${newUser.email}`
-      await redisClient.set(redisKey, hashedOtp,'EX',200)
-      await sendMail(newUser.email,otpCode)
+      await redisClient.set(redisKey, hashedOtp, 'EX', 200)
+      await sendMail(newUser.email, otpCode)
       return res.status(200).json({ message: `Success inserting the new user ${otpCode}` })
 
     } catch (error) {
@@ -34,45 +35,61 @@ export default {
 
 
   },
-  verifyUserOTP: async (req,res) =>{
-    let {email,otp} = req.body
+  verifyUserOTP: async (req, res) => {
+    let { email, otp } = req.body
     try {
       let redisKey = `otp:user:${email}`
-      let storedHashedOtp = await redisClient.get(`otp:user:${email}`)
-      if(!storedHashedOtp){
-       return res.status(404).json({message:"OTP is expired or not found"})
+      let storedHashedOtp = await redisClient.get(redisKey)
+      if (!storedHashedOtp) {
+        return res.status(404).json({ message: "OTP is expired or not found" })
       }
 
-      if ( await bcrypt.compare(otp.toString(), storedHashedOtp)) {
-      redisClient.del(redisKey)
-      const userVerificationState = true
-       await User.updateUser('isVerified', userVerificationState, email)
-      
-       return res.status(200).json({accountVerified:userVerificationState,message:'otp is verified'})
-      }else{
+      if (await bcrypt.compare(otp.toString(), storedHashedOtp)) {
+        redisClient.del(redisKey)
+        const userVerificationState = true
+        await User.updateUser('isVerified', userVerificationState, email)
+
+        return res.status(200).json({ accountVerified: userVerificationState, message: 'otp is verified' })
+      } else {
         return res.status(404).json({ accountVerified: true, message: 'wrong otp, check otp code or regenerate a new one' })
-        
+
       }
     } catch (error) {
-     return res.status(500).json({message:`Error: ${error}`})
+      return res.status(500).json({ message: `Error: ${error}` })
     }
   },
-  login: async (req,res)=>{
-    let {email,password} = req.body
+  login: async (req, res) => {
+    let { email, password } = req.body
     try {
       let userData = await User.findUserByEmail(email)
-      console.log(userData)
-      if(userData){
+      if (userData) {
         let user = new User(userData)
-        console.log(user)
-        if(await bcrypt.compare(password,user.password)){
-          const token = generateJWT({userId:user.id, verified:user.isVerified})
-          return res.status(200).json({message:"Successful login", JWT:token})
+        if (await bcrypt.compare(password, user.password)) {
+          const token = generateJWT({ userId: user.id, role: "user" })
+          //verify if the user has a refrechToken
+          let redisKey = `refrechToken:user:${email}`
+          let refreshToken = await redisClient.get(redisKey)
+          //if not
+          if (!refreshToken) {
+            refreshToken = generateSec(parseInt(process.env.LENGTH))
+            await redisClient.set(redisKey, refreshToken, 'EX', process.env.REFRESH_TIME)
+          }
+          return res.status(200).json({
+            message: "Successful login",
+            userJWT: token,
+            refreshToken: refreshToken
+          })
         }
       }
-      return res.status(200).json({message:"OK, but faild login, unfound user"})
+      return res.status(401).json({
+        status: "faild",
+        message: "Invalid Credintials"
+      })
     } catch (error) {
-      return res.status(500).json({message:`internal server error while login in: ${error}`})
+      return res.status(500).json({
+        status: "faild",
+        message: `internal server error while login in: ${error}`
+      })
     }
   }
 }
--- backend/utils/validateReq.js
+++ backend/utils/validateReq.js
@@ -1,7 +1,9 @@
 const validateReq = (req,res,next)=>{
    if (req.validatedData != null){
-    return  res.status(400).json({message:"data validation error"})
+    return  res.status(400).json({message:"data validation error",
+       error:req.validatedData})
    }
    next()
+   console.log(req.email)
 }
 export default validateReq
